f80ba24edb08ce69eff78b620549062d2eeb4d4e
lukaw3d <lukajeran@oasislabs.com>
2024-09-23 23:31:17 +0200

    Rename

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,7 +2,7 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { sapphireTestnet } from 'wagmi/chains'
 
 export const config = getDefaultConfig({
-  appName: 'ROSE On-Ramp',
+  appName: 'sapphire-stake-subcall',
   projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
   chains: [sapphireTestnet],
   ssr: false, // If your dApp uses server side rendering (SSR)
